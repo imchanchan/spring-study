8ac3d95d32268d1107ec96f80841bb3cce8c7c0e
imchanchan <chaniya918@gmail.com>
2025-01-21 16:51:35 +0900

    init

---
+++ email-auth/src/main/java/com/chan/email_auth/EmailAuthApplication.java
@@ -0,0 +1,13 @@
+package com.chan.email_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuthApplication.class, args);
+	}
+
+}
---
+++ email-auth/src/test/java/com/chan/email_auth/EmailAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.email_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
